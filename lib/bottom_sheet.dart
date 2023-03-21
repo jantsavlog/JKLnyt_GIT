8f05b03af48e6658e62a57e6d85c07d6d9bc2c52
joantiku <joantiku@student.jyu.fi>
2023-03-21 19:42:44 +0200

    dokumentaatio ja paljon korjauksia.

--- lib/bottom_sheet.dart
+++ lib/bottom_sheet.dart
@@ -11,36 +11,52 @@ class BottomSheetWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Luodaan bottom sheet, alla olevat double arvot ovat alkukoko, minimikoko,
+    // maksimikoko, sekä kohdat mihin sheetti snappaa.
     return DraggableScrollableSheet(
-      initialChildSize: 0.05,
-      minChildSize: 0.05,
+      initialChildSize: 0.038,
+      minChildSize: 0.038,
       maxChildSize: 0.9,
+      snap: true,
+      snapSizes: const [0.5],
       builder: (BuildContext context, ScrollController scrollController) {
-        return Container(
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(16),
+        // Koska appbar on nyt läpinäkyvä ja kartta venyy sen alle, pitää
+        // sisältöä muokata laskemaan asiat hieman eri tavalla.
+        return MediaQuery.removePadding(
+          context: context,
+          removeTop: true,
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Color.fromRGBO(255, 255, 255, 1.0),
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(16),
+              ),
             ),
-          ),
-          child: SingleChildScrollView(
-            controller: scrollController,
             child: Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(Icons.keyboard_arrow_up),
-                  ],
+                // Tämä widget on tässä jotta saadaan joku ankkuri bottom
+                // sheetin avaamiselle.
+                SingleChildScrollView(
+                  controller: scrollController,
+                  child: const SizedBox(
+                    height: 32,
+                    width: double.infinity,
+                    child: Icon(Icons.drag_handle_rounded),
+                  ),
                 ),
-                const SizedBox(height: 5),
-                ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: events.length,
-                  itemBuilder: (context, index) {
-                    final event = events[index];
-                    return EventTile(event: event);
-                  },
+                // Luodaan kaiken saatavilla olevan tilan täyttävä listview,
+                // joka on scrollattava luettelo widgettejä.
+                Expanded(
+                  child: Scrollbar(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: events.length,
+                      itemBuilder: (context, index) {
+                        final event = events[index];
+                        return EventTile(event: event);
+                      },
+                    ),
+                  ),
                 ),
               ],
             ),
--- lib/navbar.dart
+++ lib/navbar.dart
@@ -1,54 +1,117 @@
 import 'package:flutter/material.dart';
 
 class NavBar extends StatefulWidget {
-  final List<Map> categories;
-  final Function(int) toggleCategory;
+  final List<Map<String, dynamic>> events;
 
-  const NavBar(
-      {Key? key, required this.categories, required this.toggleCategory})
-      : super(key: key);
+  const NavBar({Key? key, required this.events}) : super(key: key);
 
   @override
   State<NavBar> createState() => _NavBarState();
 }
 
 class _NavBarState extends State<NavBar> {
+  Map<String, String> venueCategories = {};
+  Map<String, bool> categories = {};
+
+  // Perus initialize.
   @override
-  build(BuildContext context) {
+  void initState() {
+    super.initState();
+    // Luodaan mappi siitä mitä kategoriaa jokainen venue edustaa.
+    venueCategories = makeVenueCategoryMap({
+      ['lutakko', 'escape', 'paviljonki']: 'Music',
+      ['jjk', 'lohi']: 'Sports'
+    });
+    // Tässä vain luodaan mappi jota käytetään ns. "kytkimenä", onko kategoria
+    // valittu vai ei.
+    categories = makeCategoryMap();
+  }
+
+  // Tämä metodi käy läpi venueCategories mapin ja vertaa omia avainarvojaan
+  // events-listaan, jos arvot käyvät yhteen (kyseisestä paikasta löytyy
+  // tapahtuma) lisätään tapahtuman kategoria kategorialistaan (ellei se jo ole
+  // siellä).
+  Map<String, bool> makeCategoryMap() {
+    var availableCategories = <String, bool>{};
+    for (String venue in venueCategories.keys) {
+      for (Map<String, dynamic> event in widget.events) {
+        if (event['venue'].toLowerCase() == venue) {
+          if (!availableCategories.keys.contains(venueCategories[venue])) {
+            availableCategories[venueCategories[venue]!] = false;
+          }
+        }
+      }
+    }
+
+    return availableCategories;
+  }
+
+  // Kytkee tapahtuman valituksi ja pois.
+  void toggleCategory(String key) {
+    categories[key] = !categories[key]!;
+  }
+
+  // Tämä metodi rakentaa mapin tapahtumapaikoista ja niiden kategorioista,
+  // ettei sitä tarvitse tehdä täysin käsin.
+  Map<String, String> makeVenueCategoryMap(Map<List<String>, String> map) {
+    var newMap = <String, String>{};
+    for (var entry in map.entries) {
+      var keyList = entry.key;
+      for (var key in keyList) {
+        newMap[key] = entry.value;
+      }
+    }
+    return newMap;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Drawer(
       child: Column(
         children: [
+          // SafeArea estää tavaraa menemästä esim. laitteen kameran syvennöksen
+          // alle.
           SafeArea(
             child: Container(
               padding: const EdgeInsets.symmetric(vertical: 20.0),
               child: const Center(
                 child: Text(
                   'Categories',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 24),
                 ),
               ),
             ),
           ),
+          // Divider luo ohuen viivan widgettien väliin.
           const Divider(height: 0.0),
+          // Expanded varmistaa, että widget on aina juuri niin iso kuin sille
+          // on tilaa annettu.
           Expanded(
+            // Scrollbar tuo mukaan näkyvän scrollauspalkin (jos koko sisältö ei
+            // mahdu ruudulle).
             child: Scrollbar(
+              // Builder voi ottaa kourallisen elementtejä käsittelyyn ja luo
+              // niistä scrollattavan visuaalisen listan.
               child: ListView.builder(
                   padding: EdgeInsets.zero,
                   shrinkWrap: true,
-                  itemCount: widget.categories.length,
+                  itemCount: categories.length,
                   itemBuilder: (context, index) {
                     return ListTile(
-                        leading: widget.categories[index]['isChecked']
+                        leading: categories.values.elementAt(index)
                             ? const Icon(Icons.check)
                             : const Icon(Icons.add),
-                        title: Text(widget.categories[index]['name']),
+                        title: Text(categories.keys.elementAt(index)),
                         onTap: () => setState(
-                              () => widget.toggleCategory(index),
+                              () => toggleCategory(
+                                  categories.keys.elementAt(index)),
                             ));
                   }),
             ),
           ),
           const Divider(height: 0.0),
+          // Tämän containerin sisältä löytyy kielenvaihtonapit, pitää miettiä
+          // tehdäänkö niitä loppuun asti.
           Container(
             padding:
                 const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),

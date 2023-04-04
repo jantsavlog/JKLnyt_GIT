cdb01de8ca6083ef6333c16f550dbbba2e78aa92
Henri Pigg <henri.m.pigg@student.jyu.fi>
2023-04-04 16:46:28 +0300

    siivottu marker-koodia

--- lib/google_maps.dart
+++ lib/google_maps.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
